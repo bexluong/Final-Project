f7ca69950acf31204903e4a5ff38e2efb1ef4bcc
bexsekai <rebluong@gmail.com>
2016-05-12 16:55:27 +0200

    add form to index view

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20160510153704) do
+
+  create_table "calculates", force: :cascade do |t|
+    t.string   "origin"
+    t.string   "string"
+    t.string   "destination"
+    t.string   "times_per_week"
+    t.string   "integer"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
+  end
 
 end
