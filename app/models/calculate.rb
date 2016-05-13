01f9e562a0652c7e695a5b8fd48fe9bd78098aa7
bexsekai <rebluong@gmail.com>
2016-05-13 11:14:17 +0200

    add fetch method to calculate class

--- app/models/calculate.rb
+++ app/models/calculate.rb
@@ -1,25 +1,26 @@
 class Calculate < ActiveRecord::Base
   require "net/http"
 
-  API_BASE_URL = https://maps.googleapis.com/maps/api/directions/json?origin=
-def initialize(origin, destination)
-  @origin = origin
-  @destionation = destination
-end
 
 
-def fetch
-  url = "https://maps.googleapis.com/maps/api/directions/json?origin=Carrer+de+Girona,+79,+08009+Barcelona,+Spain&destination=Passatge+hort+dels+velluters,+5,+Barcelona&mode=bicycling&key=AIzaSyDaljhwqza0E_tfS7tkPupnpmDzWM47unQ"
-  uri = URI(url)
-  response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
-    request = Net::HTTP::Get.new uri
-    http.request request # Net::HTTPResponse object​
+  def initialize(origin, destination)
+    @origin = origin
+    @destionation = destination
+    @key= 'AIzaSyDaljhwqza0E_tfS7tkPupnpmDzWM47unQ'
   end
 
-  puts response.body
+  API_BASE_URL = 'https://maps.googleapis.com/maps/api/directions/json?origin=' + @origin + '&destination=' + @destination
 
+  def fetch
+    url =
+    uri = URI(url)
+    response = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+      request = Net::HTTP::Get.new uri
+      http.request request # Net::HTTPResponse object​
+    end
+    puts response.body
 
-def
-  hash = JSON.parse(response.body)
-end
-end
+  def
+    hasharray = JSON.parse(response.body)
+  end
+  end
