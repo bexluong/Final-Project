e119e29f78142fc9a3fcaee5634230e20c39d79a
bexsekai <rebluong@gmail.com>
2016-05-18 15:15:51 +0200

    add arams to calculator controller

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -2,10 +2,16 @@ class CalculatorController < ApplicationController
   def index
   end
 
-  def search
+  def search(origin, destination)
+    origin, destination = beautify_params(params[:origin], params[:destination])
     bike = Bicycle_Calculator.new
     bike.route_details
 
   end
+  
+  private
 
+  def beautify_params(origin, destination)
+    [(origin.gsub! /\s+/, '+'), (destination.gsub! /\s+/, '+')]
+  end
 end
